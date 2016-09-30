edb583e6ec31999e719028ce43e1d4a7e69f2ec2
SangQ <2014ksg@adwitt.com>
2016-09-30 15:01:50 +0900

    프로젝트 올린다.

---
+++ src/main/java/com/address/NsAddressApplication.java
@@ -0,0 +1,12 @@
+package com.address;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NsAddressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NsAddressApplication.class, args);
+	}
+}
